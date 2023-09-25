d9116de50ac9f593b49bd8af9c2b64a91bf2715c
lmthdev <146023632+lmthdev@users.noreply.github.com>
2023-09-25 21:22:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-
+import time
+print("Program Başlatılıyor...")
+print(" ")
+time.sleep(4)
+while "true":
+    
+    ad = input("Adınız ? : \n")
+    print(" ")
+    time.sleep(1)
+    yıl = int(input ("Doğum tarihiniz ? : \n"))
+    print(" ")
+    print("Lüten bekleyiniz, hesaplanıyor...")
+    sonuc = 2023 - int(yıl)
+    
+    time.sleep(3)
+    print(" ")
+    print(ad + ', yaşın ' + '"' + str(sonuc) + '"')
+    print(" ")
+    time.sleep(2)
+    print("--------------------------")
